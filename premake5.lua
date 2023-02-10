f524ece0fd3a894b57027b4a9a236ae77a9bc415
Philipp Kurth <philipp.kurth@fau.de>
2023-02-10 17:03:11 +0100

    Proper premake file

--- premake5.lua
+++ premake5.lua
@@ -8,8 +8,8 @@ project "DirectXTex"
 		"MultiProcessorCompile"
 	}
 
-	targetdir ("bin/" .. outputdir) -- TODO
-	objdir ("bin_int/" .. outputdir .. "/%{prj.name}") -- TODO
+	targetdir ("../../bin/" .. outputdir)
+	objdir ("../../bin_int/" .. outputdir .. "/%{prj.name}")
 
 	defines {
         "WIN32_LEAN_AND_MEAN",
@@ -20,8 +20,30 @@ project "DirectXTex"
     }
 
     files {
-        "DirectXTex/**.h",
-        "DirectXTex/**.cpp",
+        "DirectXTex/BC.h",
+        "DirectXTex/DDS.h",
+        "DirectXTex/DirectXTexP.h",
+        "DirectXTex/filters.h",
+        "DirectXTex/scoped.h",
+        "DirectXTex/BC.cpp",
+        "DirectXTex/BC4BC5.cpp",
+        "DirectXTex/BC6HBC7.cpp",
+        "DirectXTex/DirectXTexCompress.cpp",
+        "DirectXTex/DirectXTexConvert.cpp",
+        "DirectXTex/DirectXTexDDS.cpp",
+        "DirectXTex/DirectXTexHDR.cpp",
+        "DirectXTex/DirectXTexImage.cpp",
+        "DirectXTex/DirectXTexMipmaps.cpp",
+        "DirectXTex/DirectXTexMisc.cpp",
+        "DirectXTex/DirectXTexNormalMaps.cpp",
+        "DirectXTex/DirectXTexPMAlpha.cpp",
+        "DirectXTex/DirectXTexResize.cpp",
+        "DirectXTex/DirectXTexTGA.cpp",
+        "DirectXTex/DirectXTexUtil.cpp",
+        "DirectXTex/DirectXTexFlipRotate.cpp",
+        "DirectXTex/DirectXTexWIC.cpp",
+        "DirectXTex/d3dx12.h",
+        "DirectXTex/DirectXTexD3D12.cpp",
     }
 
     filter "system:windows"
